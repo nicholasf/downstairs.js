9684b5d805c7ec4bf488796048f765d302a010e6
Nicholas Faiz <nicholas.faiz@gmail.com>
2012-10-10 17:39:28 +1100

    we use lastval function via create

--- test/model_test.js
+++ test/model_test.js
@@ -3,7 +3,8 @@ var Downstairs = require('../lib/downstairs')
   , should = require('should')
   , sql = require('sql')
   , env = require('./../config/env')
-  , Connection = Downstairs.Connection;
+  , Connection = Downstairs.Connection
+  , helper = require('./helper');
 
 Table.use(Downstairs);
 
@@ -27,6 +28,28 @@ describe('A model can connect to the database', function() {
     Downstairs.add(myDefaultPGConnection);
     var User = Table.model('User', userSQL);
     should.exist(User.connection);
+  });
+});
+
+describe('Table functions copied to the Model', function() {
+  
+  beforeEach(function(done) {
+    helper.resetDb(helper.userTableSQL, done);
+  });
+
+
+  it('creates a new Model and returns an instance', function(done) {
+    var myDefaultPGConnection;
+    myDefaultPGConnection = new Connection.PostgreSQL(env.connectionString);
+    Downstairs.add(myDefaultPGConnection);
+    var User = Table.model('User', userSQL);
+    User.create({username: 'fred2', password: 'nottelling', email: 'test2@test.com'}, function(err, user) {
+      should.exist(user);
+      user.should.not.be.a('boolean');
+      should.exist(user.id);
+      done();
+    });
+
   });
 
 });
