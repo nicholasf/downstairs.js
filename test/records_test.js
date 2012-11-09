1f935452dde6862848745c55db4b072031e17085
Nicholas Faiz <nicholas.faiz@gmail.com>
2012-11-09 14:28:59 +1100

    validations called before save() with record

--- test/records_test.js
+++ test/records_test.js
@@ -4,14 +4,14 @@ var Downstairs = require('../lib/downstairs')
   , sql = require('sql')
   , env = require('./../config/env')
   , Connection = Downstairs.Connection
-  , helper = require('./helper');
+  , helper = require('./helper')
+  , Validator = require('validator').Validator;
 
 var pgConnection = new Downstairs.Connection.PostgreSQL(env.connectionString);
 Downstairs.add(pgConnection);
 Collection.use(Downstairs);
 
 describe('save', function() {
-
   beforeEach(function(done){
     helper.resetDb(helper.userSQL, done);
   })
@@ -30,10 +30,41 @@ describe('save', function() {
       });
     });
   });
+
+  describe('validations', function(done){
+    it('before a save', function(done){
+      var userValidations = {
+        usernamePresent: function(cb){
+          var validator =  new Validator();
+          try{
+            validator.check(this.username).notNull();
+            cb(null, null);
+          }
+          catch(e){
+            cb(null, "Username: " + e.message);
+          }
+        }
+      };
+
+      var User = Collection.model('User', helper.userConfig, userValidations);    
+      var data = {username: 'fred', email: 'fred@moneytribe.com.au'};
+
+      helper.ectypes.User.create(data, function(err, results) {
+        User.find({username: 'fred'}, function(err, user){
+          user.username = null;
+
+          user.save(function(err, user){
+            should.exist(err);
+            should.not.exist(user);
+            done();
+          });
+        });
+      });
+    });
+  });
 });
 
 describe('destroy', function() {
-
   beforeEach(function(done){
     helper.resetDb(helper.userSQL, done);
   })
